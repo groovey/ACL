ffe4e01e18c56722789989f94c7357a03e511cac
pokoot <pokoot@gmail.com>
2016-08-06 04:53:31 +0800

    More console testing

--- tests/ACLTest.php
+++ tests/ACLTest.php
@@ -1,9 +1,19 @@
 <?php
 
 use Silex\Application;
+use Symfony\Component\Console\Application as ConsoleApplication;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Console\Command\Command;
 use Groovey\ORM\Providers\ORMServiceProvider;
 use Groovey\Support\Providers\TraceServiceProvider;
 use Groovey\ACL\Providers\ACLServiceProvider;
+use Groovey\Migration\Commands\Init;
+use Groovey\Migration\Commands\Reset;
+use Groovey\Migration\Commands\Up;
+use Groovey\Migration\Commands\Down;
+use Groovey\Migration\Commands\Drop;
+use Groovey\Seeder\Commands\Init as SeedInit;
+use Groovey\Seeder\Commands\Run;
 
 class ACLTest extends PHPUnit_Framework_TestCase
 {
@@ -32,15 +42,60 @@ class ACLTest extends PHPUnit_Framework_TestCase
         ]);
 
         $app['db']->connection();
-        $app['acl']->load($userId = 1, getcwd().'/resources/yaml/permissions.yml');
 
         $this->app = $app;
     }
 
-    public function testInit()
+    public function testMigrate()
     {
-        $app   = $this->app;
+        $container['db'] = $this->app['db'];
 
+        $tester = new Tester();
+        $tester->command(new Init($container), 'migrate:init');
+        $this->assertRegExp('/Sucessfully/', $tester->getDisplay());
+
+        $tester->command(new Reset($container), 'migrate:reset', 'Y\n');
+        $this->assertRegExp('/All migration entries has been cleared/',
+                $tester->getDisplay());
+
+        $tester->command(new Up($container), 'migrate:up');
+        $this->assertRegExp('/Running migration file/', $tester->getDisplay());
+    }
+
+    public function testSeed()
+    {
+        $container['db'] = $this->app['db'];
+
+        $tester = new Tester();
+        $tester->command(new SeedInit($container), 'seed:init');
+        $this->assertRegExp('/Sucessfully/', $tester->getDisplay());
+
+        $app = new ConsoleApplication();
+        $app->add(new Run($container));
+        $command = $app->find('seed:run');
+        $tester = new CommandTester($command);
+
+        $tester->execute([
+                'command' => $command->getName(),
+                'class'   => 'Users',
+                'total'   => 5,
+            ]);
+
+        $this->assertRegExp('/End seeding/', $tester->getDisplay());
+
+        $tester->execute([
+                'command' => $command->getName(),
+                'class'   => 'Permissions',
+                'total'   => 5,
+            ]);
+
+        $this->assertRegExp('/End seeding/', $tester->getDisplay());
+    }
+
+    public function testLoad()
+    {
+        $app = $this->app;
+        $app['acl']->load($userId = 1, getcwd().'/resources/yaml/permissions.yml');
     }
 
     public function testPermissions()
@@ -83,5 +138,19 @@ class ACLTest extends PHPUnit_Framework_TestCase
         $app['acl']::setPermission('sample.view', 'value', 'deny');
         $status = deny('sample.view');
         $this->assertTrue($status);
+    }
+
+    public function testDrop()
+    {
+        $container['db'] = $this->app['db'];
+
+        $tester = new Tester();
+        $tester->command(new Down($container), 'migrate:down', 'Y\n');
+        $tester->command(new Down($container), 'migrate:down', 'Y\n');
+        $tester->command(new Down($container), 'migrate:down', 'Y\n');
+        $this->assertRegExp('/Downgrading migration file/', $tester->getDisplay());
+
+        $tester->command(new Drop($container), 'migrate:drop', 'Y\n');
+        $this->assertRegExp('/Migrations table has been deleted/', $tester->getDisplay());
     }
 }
